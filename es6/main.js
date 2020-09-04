e26a3c77ca4e86945fd4747c85df5e76fa9b161c
Kamila-Vieira <vkamila54@outlook.com>
2020-09-04 18:14:15 -0300

    Updade 'Classes' class

--- es6/main.js
+++ es6/main.js
@@ -1,1 +1,36 @@
-alert('test other')
+//Classes
+class List {
+    constructor(){
+        this.data = [];
+    }
+
+    add(data){
+        this.data.push(data);
+        console.log('3:', this.data);
+    }
+}
+class TodoList extends List{
+    constructor(){
+        super();
+        this.usuario = 'Kamila';
+    }
+
+    mostraUsuario(){
+        console.log('2:', this.usuario);
+    }
+}
+
+var minhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    minhaLista.mostraUsuario();
+    minhaLista.add('novo todo');
+}
+
+class Matematica {
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+console.log('(Pressione Adicionar) | 1:', Matematica.soma(1, 3))
