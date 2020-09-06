16dc6ee49227ef078cf509e0c314cee5059a859c
Kamila-Vieira <vkamila54@outlook.com>
2020-09-06 01:15:02 -0300

    class 'const & let'

--- es6/bundle.js
+++ es6/bundle.js
@@ -88,4 +88,22 @@ var Matematica = /*#__PURE__*/function () {
   return Matematica;
 }();
 
-console.log('(Pressione Adicionar) | 1:', Matematica.soma(1, 3));
+console.log('(Pressione Adicionar) | 1:', Matematica.soma(1, 3)); //Const & let
+//const, diferente de var não se pode reatribuir valor. só se pode mutar(dentro de objetos{} e vetores[]):
+
+var usuario = {
+  nome: 'Kamila'
+};
+usuario.nome = 'Diego';
+console.log('4:', usuario); //let, são variáveis de escopo não são visíveis fora da funcão por exemplo:
+
+function teste(x) {
+  var y = 2;
+
+  if (x > 5) {
+    console.log(x, 'e', y);
+  }
+}
+
+teste(7);
+console.log(y);
--- es6/main.js
+++ es6/main.js
@@ -34,3 +34,18 @@ class Matematica {
 }
 
 console.log('(Pressione Adicionar) | 1:', Matematica.soma(1, 3))
+
+//Const & let
+//const, diferente de var não se pode reatribuir valor. só se pode mutar(dentro de objetos{} e vetores[]):
+const usuario = {nome: 'Kamila'};
+usuario.nome = 'Diego';
+console.log('4:', usuario);
+//let, são variáveis de escopo não são visíveis fora da funcão por exemplo:
+function teste(x){
+    let y = 2;
+    if(x>5){
+        console.log(x, 'e', y);
+    }
+}
+teste(7);
+console.log(y);
