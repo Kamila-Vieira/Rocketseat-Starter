5a1c5648a8232b986910b3830896238fe895882e
Kamila-Vieira <vkamila54@outlook.com>
2020-09-07 12:51:36 -0300

    Update chalenge

---
+++ es6/desafio/index.js
@@ -0,0 +1,27 @@
+class Usuario {
+    constructor(email, senha){
+        this.email = email;
+        this.senha = senha;
+        this.admin = false;
+    }
+    
+    isAdmin(){
+        Usuario = this.admin ? true : false;
+
+    }
+}
+
+class Admin extends Usuario{
+    constructor(){
+        super();
+        this.admin = true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+console.log(Adm1);
+console.log(User1);
+
+console.log(User1.isAdmin()); // false
+console.log(Adm1.isAdmin());// true
